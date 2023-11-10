fcd6b78921bf6f8461fad38ff9de7ce76b77a298
kri_YamiHikari <1906178267@qq.com>
2023-11-10 20:00:02 +0800

    Initial commit

---
+++ src/main/java/com/example/carbonproject/CarbonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.carbonproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbonProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarbonProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/carbonproject/CarbonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carbonproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbonProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
