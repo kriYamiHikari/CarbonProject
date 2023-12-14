c8a5b7c950e955d33bf8681b1b9acd22db639142
kri_YamiHikari <1906178267@qq.com>
2023-12-15 04:46:56 +0800

    bug修复
    修复添加websocket后无法使用pom进行package的问题

--- src/main/java/com/example/carbonproject/config/WebSocketConfig.java
+++ src/main/java/com/example/carbonproject/config/WebSocketConfig.java
@@ -2,13 +2,10 @@ package com.example.carbonproject.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
-@EnableWebSocket
 public class WebSocketConfig {
 
     @Bean
